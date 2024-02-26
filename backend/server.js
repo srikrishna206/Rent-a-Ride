e72b5da8bf9ba2b461e135fc88ed944a81e4e059
jeevan-aj <ambrahamjeevan@gmail.com>
2024-02-26 12:37:32 +0530

    created a test route

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,19 @@
-import express from 'express'
-import mongoose from "mongoose"
-import dotenv from 'dotenv'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import router from "./routes/userRoute.js";
 
-dotenv.config()
+const App = express();
+dotenv.config();
+const port  = 3000
 
-mongoose.connect(process.env.mongo_uri)
-.then(console.log("connected"))
-.catch(error => console.error(error))
+mongoose
+  .connect(process.env.mongo_uri)
+  .then(console.log("connected"))
+  .catch((error) => console.error(error));
 
+App.listen(port, () => {
+  console.log("server listening !");
+});
 
-
-const App = express()
-
-App.listen(3000,()=> {
-    console.log('server listening !')
-})
+App.use('/api/user',router)
