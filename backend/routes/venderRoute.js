5aeaece526d15976438879dc0a9398cc89a7bb97
jeevan-aj <ambrahamjeevan@gmail.com>
2024-03-04 11:37:06 +0530

    vendor privte routes updated

--- backend/routes/venderRoute.js
+++ backend/routes/venderRoute.js
@@ -1,9 +1,10 @@
-import { vendorSignin, vendorSignup } from "../controllers/vendorController.js"
+import { vendorSignin, vendorSignout, vendorSignup } from "../controllers/vendorController.js"
 import express from "express"
 
 const router = express.Router()
 
 router.post('/vendorsignup',vendorSignup)
 router.post('/vendorsignin',vendorSignin)
+router.get('/vendorsignout',vendorSignout)
 
 export default router
--- client/src/components/VendorPrivateRoute.jsx
+++ client/src/components/VendorPrivateRoute.jsx
@@ -1,12 +1,15 @@
-
-import { useSelector } from 'react-redux'
-import { Navigate, Outlet } from 'react-router-dom'
-
-
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
 
 function VendorPrivateRoute() {
-  const currentUser = useSelector((state)=> state.user.currentUser)
-  return (currentUser && currentUser.isVendor) ? <Outlet/> : <Navigate to={'/vendorSignin'}/> 
+  const currentUser = useSelector((state) => state.user.currentUser);
+  console.log(currentUser);
+
+  return currentUser && currentUser.isVendor ? (
+    <Outlet />
+  ) : (
+    <Navigate to={"/vendorSignin"} />
+  );
 }
 
-export default VendorPrivateRoute
+export default VendorPrivateRoute;
