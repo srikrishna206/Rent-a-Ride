ceb153f5160de19e820432e60aa9e855d10b3a41
jeevan-aj <ambrahamjeevan@gmail.com>
2024-04-23 15:12:33 +0530

    vendor vehicle Edit , delete  done

--- backend/routes/venderRoute.js
+++ backend/routes/venderRoute.js
@@ -2,7 +2,7 @@
 import {  vendorGoogle, vendorSignin, vendorSignout, vendorSignup } from "../controllers/vendorControllers/vendorController.js"
 import express from "express"
 import { multerMultipleUploads } from "../utils/multer.js"
-import { showVendorVehicles, vendorAddVehicle } from "../controllers/vendorControllers/vendorCrudController.js"
+import { showVendorVehicles, vendorAddVehicle, vendorDeleteVehicles, vendorEditVehicles } from "../controllers/vendorControllers/vendorCrudController.js"
 
 const router = express.Router()
 
@@ -12,6 +12,9 @@ router.get('/vendorsignout',vendorSignout)
 router.post('/vendorgoogle',vendorGoogle)
 router.post('/vendorAddVehicle',multerMultipleUploads,vendorAddVehicle)
 router.post('/showVendorVehilces',showVendorVehicles)
+router.put('/vendorEditVehicles/:id',vendorEditVehicles)
+router.delete('/vendorDeleteVehicles/:id',vendorDeleteVehicles)
+
 
 
 export default router
