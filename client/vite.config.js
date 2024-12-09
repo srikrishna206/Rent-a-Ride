40df97dc1fdd6e2d07143dd11cfaa41a0c37659b
jeevan-aj <jeevan@finanshels.com>
2024-12-10 03:39:48 +0530

    changed backend url to point to onrendered.com

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy:{
       "/api":
       {
-        target:"http://localhost:3000",
+        target:"http://rent-a-ride-1.onrender.com",
         secure:true
       }
     }
