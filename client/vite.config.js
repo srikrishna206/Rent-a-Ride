6f5bc9ab9d9f0f4616df2d5a0a6579367b724f11
jeevan-aj <jeevan@finanshels.com>
2025-02-04 01:23:08 +0530

    wrong line removed

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => {
           target: env.VITE_PRODUCTION_BACKEND_URL,
           changeOrigin: true,
           secure: false,
-          rewrite: (path) => path.replace(/^\/api/, '')
+          // rewrite: (path) => path.replace(/^\/api/, '')
         }
       } : {}
     }
